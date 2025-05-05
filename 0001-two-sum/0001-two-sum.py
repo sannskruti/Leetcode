e80b013a185d0542978b0303873d5f6696f033ad
Sanskruti Manoria <manoria.s@northeastern.edu>
2025-05-05 00:30:12 -0400

    Time: 0 ms (100%), Space: 13.2 MB (61.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,22 +1,18 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        diff=0
-        numMap={}
-        for i,n in enumerate(nums):
-            diff= target-nums[i]
-            if diff in numMap:
-                return [numMap[diff],i]
-            else:
-                numMap[n]=i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        smap={}
+        for i in range (0, len(nums)):
+            diff=target-nums[i]
+            if diff in smap:
+                return [smap[diff],i]
+            smap[nums[i]]=i
 
 
-
-
-
-
-
-
-
-      
 
         
