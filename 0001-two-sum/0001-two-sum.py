f8cc5aa85278d1c4f0d6e8d486175045d057f325
Sanskruti Manoria <manoria.s@northeastern.edu>
2025-05-14 09:34:50 -0400

    Time: 0 ms (100%), Space: 18.9 MB (34.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,15 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        smap={}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        myDict={}
         for i in range (0, len(nums)):
             diff=target-nums[i]
-            if diff in smap:
-                return [smap[diff],i]
-            smap[nums[i]]=i
+            if diff in myDict:
+                return [i, myDict[diff]]
+            myDict[nums[i]]=i
+
+# I need to store key value pairs in my dict , which would be nums[i] as key and its index from num as value
+
+
 
 
 
