35aae5e1872f1172bb500302b717425a55e56da0
Sanskruti Manoria <manoria.s@northeastern.edu>
2025-05-17 15:15:46 -0400

    Time: 4 ms (53.19%), Space: 17.8 MB (44.09%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,13 +1,20 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        temp=sorted(nums)
         res=[]
+        myMap={}
+        for i,num in enumerate(temp):
+            if num not in myMap:
+                myMap[num]=i
         for i in range (0, len(nums)):
-            count=0
-            for j in range (0, len(nums)):
-                if nums[j]<nums[i]:
-                    count=count+1
-            res.append(count)
+            res.append(myMap[nums[i]])
         return res
+            
+
+
+      
+
+       
 
 
        
