be2dd74c8c695d101e569e326f42c70964718099
Sanskruti Manoria <manoria.s@northeastern.edu>
2025-05-24 18:06:23 -0400

    Time: 27 ms (6.07%), Space: 19.4 MB (62.14%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -1,9 +1,63 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        sqArr=[]
-        for i in range (0, len(nums)):
-            sqArr.append(nums[i] * nums[i])
-        return sorted(sqArr)
+        print(nums)
+        if not nums:
+            return nums
+        
+        if nums[0]>0:
+            return [i**2 for i in nums]
+        if nums[-1] < 0:
+            return [i**2 for i in nums][::-1]
+        m=0
+
+        for i in range(0, len(nums)):
+            if nums[i]>=0:
+                m=i
+                break
+            
+        # split using m
+        pos=nums[m:]
+        neg=nums[0:m]
+        print(pos,neg,m)
+        neg.reverse()
+        a,b=0,0
+        ret=[]
+        for i in range(0, len(pos)):
+            pos[i]=pos[i]*pos[i]
+
+        for i in range(0, len(neg)):
+            neg[i]=neg[i]*neg[i]
+        print(neg)
+        
+        while (a<len(pos) and b<len(neg)):
+            if (neg[b])<(pos[a]):
+                ret.append(neg[b])
+                b+=1
+            else:
+                ret.append(pos[a])
+                a+=1
+
+        while(a<len(pos)):
+            ret.append(pos[a])
+            a+=1
+
+        while (b<len(neg)):
+            ret.append(neg[b])
+            b+=1
+
+        return ret
+
+     
+       
+
+        
+
+
+
+
+
+
+      
         
  
 
