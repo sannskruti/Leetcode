34464aa978c9d1abc92c9c98f80cacbaf37413df
Sanskruti Manoria <manoria.s@northeastern.edu>
2025-09-04 10:04:27 -0400

    Time: 0 ms (100%), Space: 17.8 MB (70.4%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.py
+++ 2058-concatenation-of-array/2058-concatenation-of-array.py
@@ -1,12 +1,4 @@
-class Solution(object):
-    def getConcatenation(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        ans=[]
-        for i in range(2):
-            for n in nums:
-                ans.append(n)
-        return ans
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
         
