ce2e79944a0dc24b6d89e2bacde00c94c083038d
Sanskruti Manoria <manoria.s@northeastern.edu>
2025-10-08 14:36:34 -0400

    Time: 1 ms (45.26%), Space: 18.1 MB (14.29%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        dict={
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+
+        for c in s:
+            if c in dict:
+                if stack and stack[-1]==dict[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        if not stack:
+            return True
+        else:
+            return False
+        
