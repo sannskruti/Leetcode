04960cdd55311669cd23219b00b256daaaae3243
Sanskruti Manoria <manoria.s@northeastern.edu>
2026-01-21 00:46:51 -0500

    Time: 0 ms (100%), Space: 20.5 MB (11.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        myDict={}
+
+        mySet={}
         for i in range (0, len(nums)):
             diff=target-nums[i]
-            if diff in myDict:
-                return [i, myDict[diff]]
-            myDict[nums[i]]=i
-
-# I need to store key value pairs in my dict , which would be nums[i] as key and its index from num as value
-
+            if diff in mySet:
+                return [mySet[diff],i]
+            mySet[nums[i]]=i
 
 
 
