81896af3acd38814edc7887ba89962a0b7475d64
Sanskruti Manoria <manoria.s@northeastern.edu>
2026-02-02 00:33:19 -0500

    Time: 3 ms (75.83%), Space: 19.4 MB (10.9%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,16 +1,45 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        sorted_nums=sorted(nums)
-        freq_dict={}
+        # sorted_nums=sorted(nums)
+        # freq_dict={}
 
-        for i, num in enumerate(sorted_nums):
-            if num not in freq_dict:
-                freq_dict[num]=i
-        ans=[]
+        # for i, num in enumerate(sorted_nums):
+        #     if num not in freq_dict:
+        #         freq_dict[num]=i
+        # ans=[]
+        # for num in nums:
+        #     ans.append(freq_dict[num])
+
+        # return ans
+
+
+        count_array=[0]*101
+        # [0,1,2,1,0,0,0,0,1]
+        prefix_sum=[0]*101
+        res=[]
+
         for num in nums:
-            ans.append(freq_dict[num])
+            count_array[num]+=1
+        # print('count:',count_array)
+        
+        for i in range(0,101):
+            prefix_sum[i]=prefix_sum[i-1]+ count_array[i]
+        # print('prefix:', prefix_sum)
 
-        return ans
+        for i in range (0, len(nums)):
+            res.append(prefix_sum[nums[i]]-count_array[nums[i]])
+
+        # print("res:",res)
+
+        return res
+
+        
+
+
+        
+
+
+
 
 
 
