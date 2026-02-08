d11163f1ddf2d3642a0cc3eddd01a527cab4eaa8
Sanskruti Manoria <manoria.s@northeastern.edu>
2026-02-08 00:30:17 -0800

    Time: 19 ms (91.78%), Space: 33.8 MB (22.18%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -12,9 +12,46 @@ class Solution:
                 return num
 
 
+        # slow=0
+        # fast=0
+        # while (fast < len(nums)):
+        #     if nums[slow]==nums[fast]:
+        #         return nums[slow]
+        #     else:
+        #         slow=nums[slow]
+        #         fast=nums[nums[fast]]
+
+        
+        # for i in range (len(nums)):
+        #     num = nums[i]
+        #     while num != nums[num-1]:
+        #         old = nums[num - 1]
+        #         nums[num-1] = num
+        #         num = old
+        # print(nums)
+        # return nums[len(nums) - 1]
+
+    
+      
+        
+        
+        
+
+        
+
+        
 
 
 
 
 
- 
+
+       
+
+
+   
+
+
+
+
+  
